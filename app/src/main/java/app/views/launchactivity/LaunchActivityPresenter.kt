ee84077d800972928befa33d551156d8d1c83670
Tahmazli <namig.accenture@gmail.com>
2018-05-06 23:58:10 +0300

    Refactored LaunchActivityPresenter and handled radiobutton changes in reactive way.

--- app/src/main/java/app/views/launchactivity/LaunchActivityPresenter.kt
+++ app/src/main/java/app/views/launchactivity/LaunchActivityPresenter.kt
@@ -5,28 +5,31 @@ import android.arch.lifecycle.OnLifecycleEvent
 import android.support.annotation.IdRes
 import android.widget.RadioGroup
 import app.ext.BasePresenter
+import app.ext.log
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.subjects.BehaviorSubject
 
 @Suppress("UNUSED_PARAMETER")
 internal class LaunchActivityPresenter(private val activity: LaunchActivity) : BasePresenter() {
 
     private lateinit var disposables: CompositeDisposable
+    private lateinit var switchObserver: BehaviorSubject<Int>
 
-    val dataBinding by lazy { activity.dataBinding }
     val viewModel by lazy { activity.launchActivityViewModel }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun createDisposable() {
+        switchObserver = BehaviorSubject.createDefault(viewModel.currentStateId.get())
         disposables = CompositeDisposable()
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun initDisposables() {
-        onSwitchChanged(dataBinding.viewSwitcher, viewModel.currentStateId.get())
         disposables += observeCurrentUser()
+        disposables += observeSwitchChanges()
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
@@ -35,8 +38,16 @@ internal class LaunchActivityPresenter(private val activity: LaunchActivity) : B
     }
 
     fun onSwitchChanged(radioGroup: RadioGroup, @IdRes checkedId: Int) {
-        viewModel.currentStateId.set(checkedId)
-        activity.handleSwitchChanges(checkedId)
+        switchObserver.onNext(checkedId)
+    }
+
+    private fun observeSwitchChanges(): Disposable {
+        return switchObserver
+                .doOnNext(viewModel.currentStateId::set)
+                .subscribeBy(
+                        onNext = activity::handleSwitchChanges,
+                        onError = { it.log<LaunchActivityPresenter>("While observing switch changing.") }
+                )
     }
 
     private fun observeCurrentUser(): Disposable {
