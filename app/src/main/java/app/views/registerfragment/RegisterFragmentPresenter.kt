ad995f3cbd7a2293f7770733b632750dec8d0e1c
Tahmazli <namig.accenture@gmail.com>
2018-05-07 00:32:08 +0300

    Moved logical stuff to ViewModel from Presenter for RegisterFragment

--- app/src/main/java/app/views/registerfragment/RegisterFragmentPresenter.kt
+++ app/src/main/java/app/views/registerfragment/RegisterFragmentPresenter.kt
@@ -5,15 +5,14 @@ import android.arch.lifecycle.OnLifecycleEvent
 import android.view.View
 import app.ext.BasePresenter
 import app.ext.log
-import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.rxkotlin.subscribeBy
 
 @Suppress("UNUSED_PARAMETER")
 internal class RegisterFragmentPresenter(private val registerFragment: RegisterFragment) : BasePresenter() {
     private lateinit var disposables: CompositeDisposable
+
 
     private val viewModel by lazy { registerFragment.registerFragmentViewModel }
 
@@ -27,44 +26,24 @@ internal class RegisterFragmentPresenter(private val registerFragment: RegisterF
         disposables.clear()
     }
 
-    fun onEtEmailChanged(email: CharSequence?, start: Int, before: Int, count: Int) {
-        email?.toString()?.let {
-            viewModel.run {
-                this.email.set(it)
-                disposables += observeEmailValidation(Observable.just(it))
-            }
-        }
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun initObservers() {
+        disposables += viewModel.observeEtEmailChanges(
+                onNext = registerFragment::handleEtEmailChanges,
+                onError = { it.log<RegisterFragmentPresenter>("while validating email.") }
+        )
+        disposables += viewModel.observeEtPasswordChanges(
+                onNext = registerFragment::handleEtPasswordChanges,
+                onError = { it.log<RegisterFragmentPresenter>("While validating password.") }
+        )
     }
 
-    private fun observeEmailValidation(stream: Observable<String>): Disposable {
-        return viewModel.run {
-            isValidEmail(stream)
-                    .doOnNext(validEmail::set)
-                    .subscribeBy(
-                            onNext = registerFragment::handleEtEmailChanges,
-                            onError = { it.log<RegisterFragmentPresenter>("While validating email.") }
-                    )
-        }
+    fun onEtEmailChanged(email: CharSequence?, start: Int, before: Int, count: Int) {
+        email?.let { viewModel.emailObserver.onNext(it) }
     }
 
     fun onEtPasswordChanged(password: CharSequence?, start: Int, before: Int, count: Int) {
-        password?.toString()?.let {
-            viewModel.apply {
-                this.password.set(it)
-                disposables += observePasswordValidation(Observable.just(it))
-            }
-        }
-    }
-
-    private fun observePasswordValidation(stream: Observable<String>): Disposable {
-        return viewModel.run {
-            isValidPassword(stream)
-                    .doOnNext(validPassword::set)
-                    .subscribeBy(
-                            onNext = registerFragment::handleEtPasswordChanges,
-                            onError = { it.log<RegisterFragmentPresenter>("While validating password.") }
-                    )
-        }
+        password?.let { viewModel.passwordObserver.onNext(it) }
     }
 
     fun onRegisterButtonClicked(view: View) {
--- app/src/main/java/app/views/registerfragment/RegisterFragmentViewModel.kt
+++ app/src/main/java/app/views/registerfragment/RegisterFragmentViewModel.kt
@@ -11,6 +11,9 @@ import domain.usecase.login.ValidateEmailUseCase
 import domain.usecase.login.ValidatePasswordUseCase
 import io.reactivex.Completable
 import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.subjects.PublishSubject
 
 internal class RegisterFragmentViewModel(private val validateEmailUseCase: ValidateEmailUseCase,
                                          private val validatePasswordUseCase: ValidatePasswordUseCase,
@@ -22,11 +25,14 @@ internal class RegisterFragmentViewModel(private val validateEmailUseCase: Valid
     val passwordError = ObservableField<String?>()
     val emailError = ObservableField<String?>()
 
-    fun isValidEmail(stream: Observable<String>): Observable<Boolean> {
+    val emailObserver: PublishSubject<CharSequence> = PublishSubject.create()
+    val passwordObserver: PublishSubject<CharSequence> = PublishSubject.create()
+
+    private fun isValidEmail(stream: Observable<String>): Observable<Boolean> {
         return validateEmailUseCase.withUpstream(stream)
     }
 
-    fun isValidPassword(stream: Observable<String>): Observable<Boolean> {
+    private fun isValidPassword(stream: Observable<String>): Observable<Boolean> {
         return validatePasswordUseCase.withUpstream(stream)
     }
 
@@ -36,5 +42,33 @@ internal class RegisterFragmentViewModel(private val validateEmailUseCase: Valid
             password.get() == null -> Completable.error(InvalidPasswordException())
             else -> registerUserUseCase.get(UserModel(email = email.get()!!, password = password.get()!!))
         }
+    }
+
+    inline fun observeEtEmailChanges(crossinline onNext: (Boolean) -> Unit,
+                                     crossinline onError: (Throwable) -> Unit): Disposable {
+        return emailObserver
+                .map(CharSequence::toString)
+                .doOnNext(email::set)
+                .window(1)
+                .flatMap { isValidEmail(it) }
+                .doOnNext(validEmail::set)
+                .subscribeBy(
+                        onNext = { onNext(it) },
+                        onError = { onError(it) }
+                )
+    }
+
+    inline fun observeEtPasswordChanges(crossinline onNext: (Boolean) -> Unit,
+                                        crossinline onError: (Throwable) -> Unit): Disposable {
+        return passwordObserver
+                .map(CharSequence::toString)
+                .doOnNext(password::set)
+                .window(1)
+                .flatMap { isValidPassword(it) }
+                .doOnNext(validPassword::set)
+                .subscribeBy(
+                        onNext = { onNext(it) },
+                        onError = { onError(it) }
+                )
     }
 }
