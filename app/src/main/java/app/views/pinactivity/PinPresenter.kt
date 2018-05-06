e36d5788189baa194bb8650c42fd7433790ccd34
Tahmazli <namig.accenture@gmail.com>
2018-05-07 00:52:16 +0300

    Moved logical part of Presenter to ViewModel in PinPresenter

--- app/src/main/java/app/views/pinactivity/PinPresenter.kt
+++ app/src/main/java/app/views/pinactivity/PinPresenter.kt
@@ -4,29 +4,35 @@ import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.OnLifecycleEvent
 import android.view.View
 import app.ext.BasePresenter
+import app.ext.log
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.rxkotlin.subscribeBy
-import io.reactivex.subjects.PublishSubject
 
 @Suppress("UNUSED_PARAMETER")
 internal class PinPresenter(private val pinActivity: PinActivity) : BasePresenter() {
     private lateinit var disposables: CompositeDisposable
-
-    private lateinit var pinObserver: PublishSubject<CharSequence>
 
     val viewModel by lazy { pinActivity.pinViewModel }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun createDisposable() {
         disposables = CompositeDisposable()
-        pinObserver = PublishSubject.create()
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun initObservers() {
-        disposables += observePinChanges()
+        disposables += viewModel.observePinChanges(
+                onNext = {
+                    if (it) {
+                        disposables += viewModel.validatePin().subscribeBy(
+                                onSuccess = pinActivity::handleValidatingPin,
+                                onError = pinActivity::handleValidatingPinError
+                        )
+                    }
+                },
+                onError = { it.log<PinPresenter>("While observing pin changes.") }
+        )
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
@@ -35,25 +41,7 @@ internal class PinPresenter(private val pinActivity: PinActivity) : BasePresente
     }
 
     fun onPinValueChanged(pin: CharSequence?, start: Int, before: Int, count: Int) {
-        pin?.let { pinObserver.onNext(it) }
-    }
-
-    private fun observePinChanges(): Disposable {
-        return pinObserver
-                .map(CharSequence::toString)
-                .distinctUntilChanged()
-                .doOnNext(viewModel.pin::set)
-                .map { it.length == viewModel.pinLength }
-                .doOnNext(viewModel.isContinueButtonEnabled::set)
-                .map { it && !viewModel.isContinueButtonVisible.get() }
-                .subscribeBy(
-                        onNext = {
-                            println(it)
-                            if (it) {
-                                disposables += observeValidatingPin()
-                            }
-                        }
-                )
+        pin?.let { viewModel.pinObserver.onNext(it) }
     }
 
     fun onBtnContinueClicked(view: View) {
@@ -61,14 +49,6 @@ internal class PinPresenter(private val pinActivity: PinActivity) : BasePresente
                 .subscribeBy(
                         onComplete = pinActivity::handleRegisteringPin,
                         onError = pinActivity::handleRegisteringPinError
-                )
-    }
-
-    private fun observeValidatingPin(): Disposable {
-        return viewModel.validatePin()
-                .subscribeBy(
-                        onSuccess = pinActivity::handleValidatingPin,
-                        onError = pinActivity::handleValidatingPinError
                 )
     }
 }
--- app/src/main/java/app/views/pinactivity/PinViewModel.kt
+++ app/src/main/java/app/views/pinactivity/PinViewModel.kt
@@ -10,6 +10,9 @@ import domain.usecase.login.EnterPinUseCase
 import domain.usecase.login.ValidatePinUseCase
 import io.reactivex.Completable
 import io.reactivex.Single
+import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.subjects.PublishSubject
 
 internal class PinViewModel(private val enterPinUseCase: EnterPinUseCase,
                             private val validatePinUseCase: ValidatePinUseCase,
@@ -18,6 +21,7 @@ internal class PinViewModel(private val enterPinUseCase: EnterPinUseCase,
     val pinLength = 4
     val isContinueButtonEnabled = ObservableBoolean(false)
     val isContinueButtonVisible = ObservableBoolean(intentModel.enterPinType == EnterPinType.Register)
+    val pinObserver: PublishSubject<CharSequence> = PublishSubject.create()
 
     fun registerPin(): Completable {
         return pin.get()?.let {
@@ -29,5 +33,20 @@ internal class PinViewModel(private val enterPinUseCase: EnterPinUseCase,
         return pin.get()?.let {
             validatePinUseCase.get(it)
         } ?: Single.error(PinNotDefinedException("Pin not defined."))
+    }
+
+    inline fun observePinChanges(crossinline onNext: (Boolean) -> Unit,
+                                 crossinline onError: (Throwable) -> Unit): Disposable {
+        return pinObserver
+                .map(CharSequence::toString)
+                .distinctUntilChanged()
+                .doOnNext(pin::set)
+                .map { it.length == pinLength }
+                .doOnNext(isContinueButtonEnabled::set)
+                .map { it && !isContinueButtonVisible.get() }
+                .subscribeBy(
+                        onNext = { onNext(it) },
+                        onError = { onError(it) }
+                )
     }
 }
