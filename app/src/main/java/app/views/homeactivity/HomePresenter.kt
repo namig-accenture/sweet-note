69d3b277909fc49c78325ad54a2e546ea789791f
Namig Tahmazli <namig.accenture@gmail.com>
2018-06-21 23:18:29 +0300

    Added showing warning dialog before logging out.

--- app/src/main/java/app/views/homeactivity/HomePresenter.kt
+++ app/src/main/java/app/views/homeactivity/HomePresenter.kt
@@ -4,11 +4,13 @@ import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.OnLifecycleEvent
 import android.support.design.widget.NavigationView
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.Toolbar
 import android.view.View
 import app.ext.BasePresenter
 import app.ext.log
 import com.example.namigtahmazli.sweetnote.R
+import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.plusAssign
@@ -36,7 +38,7 @@ internal class HomePresenter(private val homeActivity: HomeActivity) : BasePrese
     val navigationItemSelectedListener = NavigationView.OnNavigationItemSelectedListener {
         when (it.itemId) {
             R.id.menu_log_out -> {
-                disposables += observeLogginUserOut()
+                disposables += observeLogoutWarningDialogAction()
                 true
             }
             else -> false
@@ -71,7 +73,30 @@ internal class HomePresenter(private val homeActivity: HomeActivity) : BasePrese
         disposables.clear()
     }
 
-    fun onAddButtonClicked(view: View) {
+    fun onAddButtonClicked(@Suppress("UNUSED_PARAMETER") view: View) {
         homeActivity.openAddEditDialog()
+    }
+
+    private fun observeLogoutWarningDialogAction(): Disposable {
+        return showLogoutWarningDialog()
+                .subscribeBy(
+                        onSuccess = { doLogout ->
+                            if (doLogout) {
+                                disposables += observeLogginUserOut()
+                            }
+                        },
+                        onError = { it.log<HomePresenter>("while observing logging out warning dialog action.") }
+                )
+    }
+
+    private fun showLogoutWarningDialog(): Single<Boolean> {
+        return Single.create<Boolean> {
+            AlertDialog.Builder(homeActivity)
+                    .setTitle(R.string.home_activity_logout_warning_dialog_title)
+                    .setMessage(R.string.home_activity_logout_warning_dialog_message)
+                    .setPositiveButton(R.string.home_activity_logout_warning_dialog_positive_action) { _, _ -> it.onSuccess(true) }
+                    .setNegativeButton(R.string.home_activity_logout_warning_dialog_negative_action) { _, _ -> it.onSuccess(false) }
+                    .show()
+        }
     }
 }
